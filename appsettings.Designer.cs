401bce132e2dcd8607de26fa1e94eb5873a22015
picartL <lpicart@B307DELL01>
2025-05-06 13:13:50 +0200

    commit for pull

--- appsettings.Designer.cs
+++ appsettings.Designer.cs
@@ -12,7 +12,7 @@ namespace HackatOrga {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     internal sealed partial class appsettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static appsettings defaultInstance = ((appsettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new appsettings())));
--- obj/Release/net8.0-windows/HackatOrga.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/HackatOrga.AssemblyInfo.cs
@@ -12,14 +12,14 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackatOrga")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c43d5dfbea49b19243a8af2c66424bb305cc167c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03481e3dcf7c9f4aae8a3a82ebf89fba3fa23c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackatOrga")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackatOrga")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows10.0.17763.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows10.0.17763.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
